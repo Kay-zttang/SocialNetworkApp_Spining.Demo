8aa7db05b04b666cda4c3ac51b5cc85a9167e6db
Ziting Tang <ziting.tang@yahoo.com>
2023-10-15 02:48:37 -0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { MainComponent } from './main/main.component';
+import { ProfileComponent } from './profile/profile.component';
+import { PostsComponent } from './main/posts/posts.component';
+import { RegisterationComponent } from './auth/registeration/registeration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    MainComponent,
+    ProfileComponent,
+    PostsComponent,
+    RegisterationComponent
   ],
   imports: [
     BrowserModule,
